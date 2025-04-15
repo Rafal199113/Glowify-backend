dd827e86dd206de0ad2a618e13e091a1e6f609cc
Rafał <rsieczkowski@asis.pl>
2025-04-15 10:07:03 +0200

    dsfds

--- config/mail.js
+++ config/mail.js
@@ -1,12 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: 'poczta.o2.pl', // Adres serwera SMTP dla o2
+    host: 'smtp.poczta.onet.pl', // Adres serwera SMTP dla o2
     port: 465, // Port SMTP (z SSL)
     secure: true, // Użyj SSL (secure: true dla portu 465)
     auth: {
-        user: 'Glowify@o2.pl', // Twój adres e-mail na o2
-        pass: 'Glowify2024@-reset', // Twoje hasło do konta
+        user: 'rafal.sieczkowski@onet.eu', // Twój adres e-mail na o2
+        pass: '@BH8%Hs,cM', // Twoje hasło do konta
     },
     tls: {
         rejectUnauthorized: false, // Wyłączenie weryfikacji certyfikatu
